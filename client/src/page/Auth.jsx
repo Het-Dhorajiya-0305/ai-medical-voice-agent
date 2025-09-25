ebf5a9a976d6e399f997303e5db8d085bdeb6e9b
Het-Dhorajiya-0305 <hetdhorajiya03@gmail.com>
2025-09-25 17:39:52 +0530

    website is ready to deploye

--- client/src/page/Auth.jsx
+++ client/src/page/Auth.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 import { toast } from 'react-toastify';
 
 function Auth() {
     const navigate = useNavigate();
 
     useEffect(() => {
-        const params = new URLSearchParams(window.location.search);
+        const [params,setParams] = useSearchParams();
         const token = params.get('refreshToken');
         if (token) {
             localStorage.setItem('refreshToken', token);
